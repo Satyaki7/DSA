bebd940f6bc42faa71b6cf5d3f1ad6db476a5d91
Satyaki Das <dsatyaki956@gmail.com>
2026-07-27 22:43:32 +0530

    121. Best Time to Buy and Sell Stock solved

---
+++ BestTime.java
@@ -0,0 +1,25 @@
+public class BestTime {
+    
+    public int maxProfit(int[] prices) {
+        int bp = prices[0], p, profit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            p = prices[i];
+            if (bp > p) {
+                bp = p;
+                System.out.println(bp);
+            }
+            profit = (profit > p - bp) ? profit : p-bp;
+        }
+        return  profit;
+    }
+    
+
+    public static void main(String[] args) {
+        int nums1[] = {3,2,6,5,0,3};
+        // int m = 7; 
+        // int[] nums2 = {3,6,7,8}; 
+        // int n = 4;
+        BestTime obj = new BestTime();
+        System.out.println(obj.maxProfit(nums1));
+    }
+}
